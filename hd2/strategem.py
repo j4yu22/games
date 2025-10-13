395ae795a2c235495ca5bf3e19f84eb5b061c856
jayunderwood <jay.e.underwood@gmail.com>
2025-10-13 13:44:29 -0600

    started strategem game, completed inaccurate codes.json list of all strategems

---
+++ hd2/strategem.py
@@ -0,0 +1,17 @@
+def round():
+    # each code is a round
+    pass
+
+def play():
+    # logic of the user typing in the code
+    pass
+
+def get_code():
+    # get a code from codes.json and return a string of the parsed code
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
